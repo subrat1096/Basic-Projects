7ec2d57071e9ec1a1a33ec2d60abc05f7f0f6344
Subrat Kumar Behera <118833161+subrat1096@users.noreply.github.com>
2023-05-16 11:00:25 +0530

    toggle functionality added

--- 15 JavaScript Projects/4 - Navbar/setup/app.js
+++ 15 JavaScript Projects/4 - Navbar/setup/app.js
@@ -9,9 +9,14 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  if (links.classList.contains("show-links")) {
-    links.classList.remove("show-links");
-  } else {
-    links.classList.add("show-links");
-  }
+
+  // using if else statement 
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+
+  // toggle functionality 
+  links.classList.toggle("show-links")
 });
