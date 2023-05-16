b3763ed7a83857005cbfeae440ff9b3af16f5792
Subrat Kumar Behera <118833161+subrat1096@users.noreply.github.com>
2023-05-16 18:29:19 +0530

    description added

--- 15 JavaScript Projects/6 - Modal/setup/app.js
+++ 15 JavaScript Projects/6 - Modal/setup/app.js
@@ -1,11 +1,14 @@
-const btn = document.querySelector('.modal-btn')
-const closeBtn = document.querySelector('.close-btn')
-const modal = document.querySelector('.modal-overlay')
+//select modal-btn, close-btn, modal-overlay
+const btn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal-overlay");
 
-btn.addEventListener('click', function () {
-    modal.classList.add("open-modal")
-})
+//add click events on modal-btn with a callback function when user clicks add open-modal class to modal-overlay
+btn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
 
-closeBtn.addEventListener('click',function () {
-    modal.classList.remove("open-modal")
-})
+//add click events on close-btn with a callback function when user clicks remove open-modal class to modal-overlay
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
